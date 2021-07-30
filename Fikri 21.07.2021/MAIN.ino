d953988a701a5399116e3abb6c30727de919de01
FikriZaini <73908282+FikriZaini@users.noreply.github.com>
2021-07-30 21:11:30 +0700

    Update MAIN.ino

--- Fikri 21.07.2021/MAIN.ino
+++ Fikri 21.07.2021/MAIN.ino
@@ -1,22 +1,38 @@
+// Inititalize library 
+// Ps3Controller.h to functionalize ps3 controller
+// ESP32Servo.h to controlling servo
 #include <Ps3Controller.h>
 #include <ESP32Servo.h>
 
+// declaration of object servo
+// servo1 for vertical movement
+// servo2 for horizontal movement
+// servo3 for gripper 90 deg rotation
 Servo myservo1;
 Servo myservo2;
 Servo myservo3;
+
+// variables for Ps3Controller
 int player = 0;
 int battery = 0;
 
+// define pin encoder
+const byte leftencoderA = 6;
+const byte leftencoderB = 7;
+const byte rightencoderA = 3;
+const byte rightencoderB = 4;
+
+// define pin IBT motor driver
 const byte ML_A = 32;
 const byte ML_B = 33;
 const byte MR_A = 25;
-const byte MR_B = 26;
+const byte MR_B = 27;
 
+// define pin servo
 const byte servoPin1 = 14;
 const byte servoPin2 = 12;
 const byte servoPin3 = 13;
 
-static int flagxservo;
 static int flagxservo90;
 
 static int velocitymotor; 
@@ -34,12 +50,9 @@ void setup()
     //initmotor();
     initPS3Controller();
     initservo();
-    /*
-    pinMode(2, OUTPUT);
-    pinMode(4, OUTPUT);
-    pinMode(12, OUTPUT);
-    pinMode(13, OUTPUT);
-    */
+    //leftinitRPM();
+    //rightinitRPM();
+
     velocitymotor = 80;
 }
 
